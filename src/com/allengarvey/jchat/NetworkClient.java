9760a45d39262ab6480ad2734cca3cf2a91c5742
Allen Garvey <allen.garvey@gmail.com>
2016-10-29 15:12:27 -0400

    Add documentation to network client class

--- src/com/allengarvey/jchat/NetworkClient.java
+++ src/com/allengarvey/jchat/NetworkClient.java
@@ -10,8 +10,11 @@ import java.net.Socket;
  * Created by allen on 10/29/16.
  */
 public class NetworkClient{
+    //socket to send and receive data
     private Socket connectionSocket;
+    //username of client
     private String userName;
+    //index of last message suceesfull broadcast
     private int currentMessageIndex;
     //shared flag between threads so that both threads have exited while loop
     //before closing connection
@@ -19,6 +22,7 @@ public class NetworkClient{
 
     public NetworkClient(Socket tcpSocket){
         this.connectionSocket = tcpSocket;
+        //initialize variables
         currentMessageIndex = 0;
         userName = null;
         isQuitting = false;
@@ -28,6 +32,10 @@ public class NetworkClient{
         return connectionSocket;
     }
 
+    //since two threads are potentially writing to and reading from username,
+    //need combined synchronized method
+    //calling with username as null is the same as getter, having a string username
+    //attempts to set user name if it is not taken
     private synchronized String getSetUserName(String userName){
         if(userName != null){
            //remove extra whitespace
@@ -41,10 +49,16 @@ public class NetworkClient{
         return this.userName;
     }
 
+    //checks if username has been set
+    //returns false if username attempt fails because it is taken
     public  boolean isUserNameInitialized(){
         return getSetUserName(null) != null;
     }
 
+    //listens for user input
+    //if user doesn't have username, attempts to set username
+    //if that succeeds, sends username back, otherwise sends error
+    //otherwise add user message to queue
     public void listenAction(){
         try {
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
@@ -64,11 +78,13 @@ public class NetworkClient{
                         outToClient.writeBytes("ERROR: "  + clientData + " is not available\n");
                     }
                     else{
+                        //username setting succeeded, send it back
                         outToClient.writeBytes("UNAME: " + userName + "\n");
                     }
 
                 }
                 else{
+                    //add message to queue
                     Main.addGetNewMessages(new ChatMessage(userName, clientData), -1);
                 }
             }
@@ -80,17 +96,22 @@ public class NetworkClient{
             isQuitting = true;
         }
     }
+    //loop that gets unread messages since last checked and broadcasts them to client
     public void broadcastAction(){
         try {
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            //check if connection needs to be shut down, because client sent \quit
             while(!isQuitting){
+                //don't send messages until client has a username
                 if(!isUserNameInitialized()){
                     continue;
                 }
+                //get all unread messages, broadcast them, and increment index
                 else{
                     ChatMessage[] unreadMessages = Main.addGetNewMessages(null, currentMessageIndex);
                     for(ChatMessage message : unreadMessages){
                         outToClient.writeBytes(message.toString());
+                        //message now broadcast, so increment index
                         currentMessageIndex++;
                     }
                 }
