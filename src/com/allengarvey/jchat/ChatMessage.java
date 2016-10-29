befa07295c0403b0b853208247bb13c265bbfc68
Allen Garvey <allen.garvey@gmail.com>
2016-10-29 15:13:59 -0400

    Add documentation to ChatMessage class

--- src/com/allengarvey/jchat/ChatMessage.java
+++ src/com/allengarvey/jchat/ChatMessage.java
@@ -3,6 +3,8 @@ package com.allengarvey.jchat;
 /**
  * Created by allen on 10/29/16.
  */
+//container for chat messages
+//keeps track of message body and sender
 public class ChatMessage {
     private String userName;
     private String messageBody;
@@ -12,6 +14,7 @@ public class ChatMessage {
         this.messageBody = messageBody;
     }
 
+    //formats message when used for sending
     @Override
     public String toString() {
         //replace extra newlines at end of message and make sure message ends in newline
